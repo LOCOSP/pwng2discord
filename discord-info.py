60a5d3847f164525a0ed1187e761af0098770777
LOCOSP <gitlab@locosp.org>
2024-09-22 03:26:00 +0200

    Big thaks to NeonLightning

--- discord-info.py
+++ discord-info.py
@@ -3,7 +3,6 @@ import os
 import requests
 from pwnagotchi import plugins
 
-
 class DiscordInfo(plugins.Plugin):
     __author__ = 'LOCOSP'
     __version__ = '1.0.0'
@@ -14,17 +13,17 @@ class DiscordInfo(plugins.Plugin):
         self.ready = False
 
     def on_loaded(self):
-
+        # Check if the plugin is enabled in the config
         if 'enabled' not in self.options or not self.options['enabled']:
             logging.info("DiscordInfo: Plugin is disabled in the config")
             return
 
-
+        # Check if webhook_url is provided in config
         if 'webhook_url' not in self.options or not self.options['webhook_url']:
             logging.error("DiscordInfo: Webhook URL is not set, cannot post to Discord")
             return
         
-
+        # Set the username from options or default to hostname
         if 'username' not in self.options or not self.options['username']:
             with open('/etc/hostname') as fp:
                 self.options['username'] = fp.read().strip()
@@ -33,27 +32,24 @@ class DiscordInfo(plugins.Plugin):
         logging.info("DiscordInfo: Plugin loaded and ready")
 
     def on_handshake(self, agent, filename, access_point, client_station):
-        if not self.ready or not self.is_enabled():
+        logging.info("DiscordInfo: handshake received")
+        if self.ready and self.is_enabled():
+            ssid = access_point["hostname"]
+            bssid = access_point["mac"]
+            message = f"New handshake captured for SSID: {ssid} (BSSID: {bssid})\nFile: {filename}"
+            logging.info(f"DiscordInfo: Sending message to Discord: {message}")
+            self.send_message(message)
+        else:
             logging.warning("DiscordInfo: Plugin not ready or disabled, cannot send message")
-            return
-
-
-        ssid = access_point["hostname"]
-        bssid = access_point["mac"]
-        message = f"New handshake captured for SSID: {ssid} (BSSID: {bssid})\nFile: {filename}"
-
-        logging.info(f"DiscordInfo: Sending message to Discord: {message}")
-
-        self.send_message(message)
 
     def is_enabled(self):
-
+        # Check if the plugin is enabled in the config
         return 'enabled' in self.options and self.options['enabled']
 
     def send_message(self, message):
         try:
             url = self.options['webhook_url']
-            username = self.options['username']  
+            username = self.options['username']  # Use the configurable username
             data = {
                 "content": message,
                 "username": username
@@ -69,4 +65,5 @@ class DiscordInfo(plugins.Plugin):
             logging.exception(f"DiscordInfo: Unexpected exception occurred: {e}")
 
     def on_unload(self):
+        self.ready = False
         logging.info("DiscordInfo: Plugin unloaded")
